7ed7d0f82cbaa2866d9779ff68d5ded4ebf04d90
Ashutosh Jare <ashuoshjare.dev@gmail.com>
2024-06-09 22:01:02 +0530

    - revised concepts: string interpolation, property binding, event binding, 2 way data binding

--- project1/src/app/app.component.ts
+++ project1/src/app/app.component.ts
@@ -3,8 +3,16 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'project1';
+
+  data = 'Some data';
+  inputValue: string = '';
+  inputValue2: string = '';
+
+  updateTextValue(event: Event) {
+    this.inputValue = (<HTMLInputElement>event.target).value;
+  }
 }
--- project1/src/app/app.module.ts
+++ project1/src/app/app.module.ts
@@ -1,18 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms'; 
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    FormsModule,
+    MatSlideToggleModule
   ],
   providers: [],
   bootstrap: [AppComponent]
